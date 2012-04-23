9f80ee00d5d65f4ffebab4d4ed0ffd580b8c8bb9
Trung Nguyen <trung.nguyen@paymium.com>
2012-04-23 16:30:14 +0200

    first commit

---
+++ src/com/paymium/instawallet/InstawalletActivity.java
@@ -0,0 +1,34 @@
+package com.paymium.instawallet;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+import android.os.Bundle;
+
+
+public class InstawalletActivity extends SherlockActivity 
+{
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) 
+	{
+        //Used to put dark icons on light action bar
+        boolean isLight = SampleList.THEME == R.style.Theme_Sherlock_Light;
+
+        menu.add("Refresh")
+            .setIcon(isLight ? R.drawable.ic_refresh_inverse : R.drawable.ic_refresh)
+            .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        setTheme(SampleList.THEME); //Used for theme switching in samples
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+
+}
